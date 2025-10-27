140948f746c09c99bacfc93184ae0909e121b3cc
Dayo <iamdayoaderinto@gmail.com>
2025-10-27 20:06:06 +0100

    Modify vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -9,22 +9,45 @@ resource "aws_vpc" "main_vpc" {
 
 }
 
-resource "aws_subnet" "public_subnet" {
+resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.main_vpc.id
   cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.azs.names[0]
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.tags["Project"]}-public-subnet"
+    Name = "${var.tags["Project"]}-public-subnet_1"
   }
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.2.0/24"
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zones.azs.names[1]
+  map_public_ip_on_launch = true
 
   tags = {
-    Name = "${var.tags["Project"]}-private-subnet"
+    Name = "${var.tags["Project"]}-public-subnet_2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main_vpc.id
+  availability_zone = data.aws_availability_zones.azs.names[0]
+  cidr_block        = "10.0.3.0/24"
+
+  tags = {
+    Name = "${var.tags["Project"]}-private-subnet_1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main_vpc.id
+  availability_zone = data.aws_availability_zones.azs.names[1]
+  cidr_block        = "10.0.4.0/24"
+
+  tags = {
+    Name = "${var.tags["Project"]}-private-subnet_2"
   }
 }
 
@@ -46,7 +69,12 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public_rt_assoc" {
-  subnet_id      = aws_subnet.public_subnet.id
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_rt_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
   route_table_id = aws_route_table.public_rt.id
 }
 
@@ -63,7 +91,7 @@ resource "aws_eip" "nat_eip" {
 
 resource "aws_nat_gateway" "nat_gw" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet.id
+  subnet_id     = aws_subnet.public_subnet_1.id
 
   tags = {
     Name = "${var.tags["Project"]}-nat-gw"
@@ -80,9 +108,12 @@ resource "aws_route_table" "private_rt" {
 }
 
 resource "aws_route_table_association" "private_rt_assoc" {
-  subnet_id      = aws_subnet.private_subnet.id
+  subnet_id      = aws_subnet.private_subnet_1.id
   route_table_id = aws_route_table.private_rt.id
 }
-
+resource "aws_route_table_association" "private_rt_assoc_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_rt.id
+}
 
 
